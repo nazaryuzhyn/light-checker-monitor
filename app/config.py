3403fe83b22ba6bb325ecd76102c81d4e4058f44
nazaryuzhyn <nazaryuzhyn@gmail.com>
2026-02-24 23:20:21 +0200

    Ignore extra fields in `Settings` model configuration.

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     ESP_API_KEY: str
     PING_TIMEOUT: int = 60
 
-    model_config = {"env_file": ".env"}
+    model_config = {"env_file": ".env", "extra": "ignore"}
 
 
 settings = Settings()
